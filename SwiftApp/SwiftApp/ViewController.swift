e0c6dae7f920a61333d0a5a8598d37e27469eb88
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-06 09:16:51 +0000

    Initial empty iOS storyboard app

---
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Nigel Barber on 06/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

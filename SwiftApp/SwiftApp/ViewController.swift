3bf09cd02e8dec3350af25e9a0946a110b214e9b
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-06 11:13:02 +0000

    ViewController - Subclass UITableViewController

--- SwiftApp/SwiftApp/ViewController.swift
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .lightGray
     }
 
 

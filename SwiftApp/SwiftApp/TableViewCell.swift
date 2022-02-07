f1885cf41e454fab01b08ca922c1ba32a62642eb
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-07 15:06:44 +0000

    constrainToSuperview() - Add insets parameter

--- SwiftApp/SwiftApp/TableViewCell.swift
+++ SwiftApp/SwiftApp/TableViewCell.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 extension UIView {
-    func constrainToSuperview() {
+    func constrainToSuperview(insets: UIEdgeInsets = .zero) {
         guard let superView = self.superview else { return }
         NSLayoutConstraint.activate([
-            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
-            trailingAnchor.constraint(equalTo: superView.trailingAnchor),
-            topAnchor.constraint(equalTo: superView.topAnchor),
-            bottomAnchor.constraint(equalTo: superView.bottomAnchor)
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: insets.left),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -insets.right),
+            topAnchor.constraint(equalTo: superView.topAnchor, constant: insets.top),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -insets.bottom)
         ])
     }
 }
@@ -30,7 +30,7 @@ class TableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.contentView.addSubview(label)
-        label.constrainToSuperview()
+        label.constrainToSuperview(insets: UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8))
     }
     
     required init?(coder: NSCoder) {

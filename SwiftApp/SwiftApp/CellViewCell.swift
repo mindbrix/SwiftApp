dfb5dc54b31412749ad99da97462736234559365
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-08 11:51:40 +0000

    CellViewCell - Promote init()

--- SwiftApp/SwiftApp/CellViewCell.swift
+++ SwiftApp/SwiftApp/CellViewCell.swift
@@ -10,12 +10,6 @@ import UIKit
 class CellViewCell: UITableViewCell {
     static let reuseID = String(describing: CellViewCell.self)
     
-    lazy var cellView: CellView = {
-        let cellView = CellView()
-        cellView.translatesAutoresizingMaskIntoConstraints = false
-        return cellView
-    }()
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.contentView.addSubview(cellView)
@@ -25,4 +19,10 @@ class CellViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    lazy var cellView: CellView = {
+        let cellView = CellView()
+        cellView.translatesAutoresizingMaskIntoConstraints = false
+        return cellView
+    }()
 }

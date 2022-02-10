a33cacdb288ce5a4afd7c54bcd0fa64528c02188
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-10 14:14:48 +0000

    TableViewController - Add monosection flag

--- SwiftApp/TableViewController.swift
+++ SwiftApp/TableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    let monosection = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(CellViewCell.self, forCellReuseIdentifier: CellViewCell.reuseID)
@@ -37,26 +39,51 @@ class TableViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        model.sections.count
+        monosection ? min(1, model.sections.count) : model.sections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        model.sections[section].cells.count
+        if monosection {
+            var count = 0
+            for section in model.sections {
+                count += section.cells.count + 0
+            }
+            return count
+        } else {
+            return model.sections[section].cells.count
+        }
     }
 
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let cv = CellView()
-        cv.apply(cell: model.sections[section].header, style: model.style)
+        cv.apply(
+            cell: monosection ? .header(caption: "monosection") : model.sections[section].header,
+            style: model.style)
         return cv
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = model.sections[indexPath.section].cells[indexPath.row]
         let tvc = tableView.dequeueReusableCell(withIdentifier: CellViewCell.reuseID, for: indexPath)
         tvc.selectionStyle = .none
-        
         if let tvc = tvc as? CellViewCell {
+            let cell: Cell = {
+                if monosection {
+                    var base = 0
+                    for section in model.sections {
+                        let end = base + section.cells.count
+                        if indexPath.row < end {
+                            return section.cells[indexPath.row - base]
+                        } else {
+                            base = end
+                        }
+                    }
+                    return model.sections[indexPath.section].cells[indexPath.row]
+                } else {
+                    return model.sections[indexPath.section].cells[indexPath.row]
+                }
+            }()
             tvc.cellView.apply(cell: cell, style: model.style)
+            
         }
         return tvc
     }

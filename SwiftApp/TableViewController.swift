36838fc78c7655c38a8e54a838a9afccf6bed5e5
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-10 18:07:13 +0000

    TableViewController - Remove monosection, withheader

--- SwiftApp/TableViewController.swift
+++ SwiftApp/TableViewController.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    let monosection = false
-    let withheader = true
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(CellViewCell.self, forCellReuseIdentifier: CellViewCell.reuseID)
@@ -40,21 +37,17 @@ class TableViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        monosection ? min(1, model.sections.count) : model.sections.count
+        model.sections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if monosection {
-            return (withheader ? model.sections.count : 0) + model.sections.reduce(0, { x, section in  x + section.cells.count })
-        } else {
-            return model.sections[section].cells.count
-        }
+        return model.sections[section].cells.count
     }
 
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let cv = CellView()
         cv.apply(
-            cell: monosection ? .header(caption: model.title) : model.sections[section].header,
+            cell: model.sections[section].header,
             style: model.style)
         return cv
     }
@@ -63,25 +56,9 @@ class TableViewController: UITableViewController {
         let tvc = tableView.dequeueReusableCell(withIdentifier: CellViewCell.reuseID, for: indexPath)
         tvc.selectionStyle = .none
         if let tvc = tvc as? CellViewCell {
-            let cell: Cell? = {
-                if monosection {
-                    var base = 0
-                    for section in model.sections {
-                        let end = base + section.cells.count + (withheader ? 1 : 0)
-                        if indexPath.row < end {
-                            let idx = indexPath.row - base
-                            return !withheader ? section.cells[idx] : idx == 0 ? section.header : section.cells[idx - 1]
-                        } else {
-                            base = end
-                        }
-                    }
-                    return nil
-                } else {
-                    return model.sections[indexPath.section].cells[indexPath.row]
-                }
-            }()
-            tvc.cellView.apply(cell: cell, style: model.style)
-            
+            tvc.cellView.apply(
+                cell: model.sections[indexPath.section].cells[indexPath.row],
+                style: model.style)
         }
         return tvc
     }

ade1e24cb6dd815e48b969ef769679da1ce49d34
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-15 20:17:49 +0000

    Atom, Cell - Align case var labels

--- SwiftApp/ViewModel.swift
+++ SwiftApp/ViewModel.swift
@@ -10,9 +10,16 @@ import UIKit
 
 
 enum Atom: Hashable {
-    case text(_ string: String, style: TextStyle? = nil, onTap: (() -> Void)? = nil)
-    case image(_ image: UIImage, width: CGFloat? = nil, onTap: (() -> Void)? = nil)
-    case input(_ value: String, placeholder: String? = nil, style: TextStyle? = nil, onSet: ((String) -> Void)? = nil)
+    case text(_ string: String,
+            style: TextStyle? = nil,
+            onTap: (() -> Void)? = nil)
+    case image(_ image: UIImage,
+            width: CGFloat? = nil,
+            onTap: (() -> Void)? = nil)
+    case input(_ value: String,
+            placeholder: String? = nil,
+            style: TextStyle? = nil,
+            onSet: ((String) -> Void)? = nil)
     
     static func == (lhs: Atom, rhs: Atom) -> Bool {
         lhs.hashValue == rhs.hashValue
@@ -23,7 +30,9 @@ enum Atom: Hashable {
 }
 
 enum Cell: Equatable {
-    case stack(_ atoms: [Atom], isVertical: Bool = false, inset: UIEdgeInsets? = nil)
+    case stack(_ atoms: [Atom],
+        isVertical: Bool = false,
+        inset: UIEdgeInsets? = nil)
 }
 
 struct Section: Equatable {

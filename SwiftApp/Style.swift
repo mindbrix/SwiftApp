c1b08139c2fef4804cf7df166f82c8ef8ae1369f
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-16 12:00:13 +0000

    Style.swift - Formatting

--- SwiftApp/Style.swift
+++ SwiftApp/Style.swift
@@ -8,37 +8,45 @@
 import Foundation
 import UIKit
 
+
 struct CellStyle: Equatable {
     let color: UIColor
     let isVertical: Bool
     let insets: UIEdgeInsets?
+    
     init(color: UIColor = .white, isVertical: Bool = false, insets: UIEdgeInsets? = nil) {
         self.color = color
         self.isVertical = isVertical
         self.insets = insets
     }
+    
     func withColor(_ color: UIColor) -> Self {
         return Self(color: color, isVertical: isVertical, insets: insets)
     }
 }
 
+
 struct TextStyle: Equatable {
     let color: UIColor
     let font: UIFont?
     let alignment: NSTextAlignment
+    
     init(color: UIColor = .black, font: UIFont? = nil, alignment: NSTextAlignment = .left) {
         self.color = color
         self.font = font
         self.alignment = alignment
     }
+    
     func withAlignment(_ alignment: NSTextAlignment) -> Self {
         return Self(color: color, font: font, alignment: alignment)
     }
 }
 
+
 struct Style: Equatable  {
     let cell: CellStyle
     let text: TextStyle
+    
     init(cell: CellStyle = .init(), text: TextStyle = .init()) {
         self.cell = cell
         self.text = text

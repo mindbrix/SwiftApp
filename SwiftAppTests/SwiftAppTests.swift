0c28b5d4f4889ad336533dd44c40f4420117fad4
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-17 09:35:12 +0000

    SwiftAppTests.testModel() - Add image comparison tests

--- SwiftAppTests/SwiftAppTests.swift
+++ SwiftAppTests/SwiftAppTests.swift
@@ -18,14 +18,23 @@ class SwiftAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testModel() throws {
         let c0 = Cell.stack([.text("Title0")])
         let c1 = Cell.stack([.text("Title1")])
         assert("\(c0)" == String(describing: c0))
         XCTAssertEqual(c0, c0)
         XCTAssertNotEqual(c0, c1)
+        
+        let img0 = UIImage(systemName: "plus.circle") ?? UIImage()
+        let img1 = UIImage(systemName: "minus.circle") ?? UIImage()
+        assert(img0 != img1)
+        
+        guard let cg0 = img0.cgImage else { return }
+        let cgImage0 = UIImage(cgImage: cg0)
+        let cgImage1 = UIImage(cgImage: cg0)
+        print("\(cgImage0.hashValue)")
+        print("\(cgImage1)")
+        assert(cgImage0 == cgImage1)
     }
 
     func testPerformanceExample() throws {

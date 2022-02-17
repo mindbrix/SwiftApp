4af1ce95c752b5ad4d7f52d4a1f41580f3af60cb
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-17 16:22:07 +0000

    TableViewController - OCD

--- SwiftApp/TableViewController.swift
+++ SwiftApp/TableViewController.swift
@@ -12,6 +12,7 @@ class TableViewController: UITableViewController {
         self.loadClosure = loadClosure
         super.init(style: .plain)
     }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

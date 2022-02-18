b98cb064391940974d9d78da0ecca03e558f3994
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-18 07:55:25 +0000

    ViewModel.description() - Remove style: label typo

--- SwiftApp/ViewModel.swift
+++ SwiftApp/ViewModel.swift
@@ -59,7 +59,7 @@ extension ViewModel {
                     header: .stack([.text(String(describing: section.header))]),
                     cells: section.cells.map({ cell in
                         switch cell {
-                        case .stack(let atoms, style: let style):
+                        case .stack(let atoms, let style):
                             return Cell.stack(
                                 atoms.map({ atom in
                                     Atom.text("\(atom.hashValue)\n\n" + String(describing: atom))

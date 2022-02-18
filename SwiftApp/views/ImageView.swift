d17b84c869b777db61161c7c408da4575b7143d8
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-18 07:37:01 +0000

    ImageView.apply() - Absorb setAspectImage()

--- SwiftApp/views/ImageView.swift
+++ SwiftApp/views/ImageView.swift
@@ -9,21 +9,6 @@ import Foundation
 import UIKit
 
 class ImageView: UIImageView, AtomAView {
-    func setAspectImage(_ image: UIImage, width: CGFloat? = nil) {
-        self.image = image
-        contentMode = .scaleAspectFit
-        heightConstraint?.isActive = false
-        widthConstraint.isActive = false
-        let size = image.size
-        guard size.height > 0 && size.width > 0 else { return }
-        heightConstraint = heightAnchor.constraint(
-            lessThanOrEqualTo: widthAnchor,
-            multiplier: size.height / size.width)
-        heightConstraint?.isActive = true
-        widthConstraint.constant = width ?? 0
-        widthConstraint.isActive = width != nil
-    }
-    
     var heightConstraint: NSLayoutConstraint?
     
     lazy var widthConstraint: NSLayoutConstraint = {
@@ -33,7 +18,18 @@ class ImageView: UIImageView, AtomAView {
     func apply(_ atom: Atom, modelStyle: ModelStyle) {
         switch atom {
         case .image(let image, let width, let onTap):
-            setAspectImage(image, width: width)
+            self.image = image
+            contentMode = .scaleAspectFit
+            heightConstraint?.isActive = false
+            widthConstraint.isActive = false
+            let size = image.size
+            guard size.height > 0 && size.width > 0 else { return }
+            heightConstraint = heightAnchor.constraint(
+                lessThanOrEqualTo: widthAnchor,
+                multiplier: size.height / size.width)
+            heightConstraint?.isActive = true
+            widthConstraint.constant = width ?? 0
+            widthConstraint.isActive = width != nil
             
             if image.isSymbolImage {
                 tintColor = onTap == nil ? modelStyle.text.color : .blue

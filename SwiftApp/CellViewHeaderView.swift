ff7ab2b8f1de0590f04f2b32195789d2092b5b73
Nigel Barber <nigel@mindbrix.co.uk>
2022-02-25 19:41:49 +0000

    CellViewHeaderView - Spacing

--- SwiftApp/CellViewHeaderView.swift
+++ SwiftApp/CellViewHeaderView.swift
@@ -22,6 +22,7 @@ class CellViewHeaderView: UITableViewHeaderFooterView {
             self.contentView.edgeConstraints(for: cellView)
         )
     }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

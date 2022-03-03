8cb50f002b8dad704750b51cbb34bab54960b1e1
Nigel Barber <nigel@mindbrix.co.uk>
2022-03-03 17:57:11 +0000

    visibleSectionIndices() - Formattting

--- SwiftApp/extensions/UITableView+visibleSectionIndices.swift
+++ SwiftApp/extensions/UITableView+visibleSectionIndices.swift
@@ -11,11 +11,14 @@ extension UITableView {
     func visibleSectionIndices(count: Int) -> [Int] {
         let visible = CGRect(
             origin: contentOffset,
-            size: bounds.size)
-    
+            size: bounds.size
+        )
         return Array(0 ..< count).filter({ i in
-            let rect = style == .plain ? rect(forSection: i) : rectForHeader(inSection: i)
-            return visible.intersects(rect)
+            visible.intersects(
+                style == .plain ?
+                    rect(forSection: i) :
+                    rectForHeader(inSection: i)
+            )
         })
     }
 }
